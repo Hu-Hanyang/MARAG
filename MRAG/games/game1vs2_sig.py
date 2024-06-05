57652e052f3db2db4d215df413273bc54aa26568
hha160 <HuHY97@outlook.com>
2024-06-05 12:19:04 -0700

    load former version codes to test 1vs2 performance, it seems that the accuracy of the pos and the controls lead to the difference

--- MRAG/games/game1vs2_sig.py
+++ MRAG/games/game1vs2_sig.py
@@ -23,14 +23,16 @@ total_steps = int(T * ctrl_freq)
 game = ReachAvoidGameEnv(num_attackers=num_attackers, num_defenders=num_defenders, 
                          initial_attacker=initial_attacker, initial_defender=initial_defender, 
                          ctrl_freq=ctrl_freq)
-
+defenders_controls = []
+attackers_controls = []
 #### Game Loop ####
 print(f"================ The game starts now. ================")
-counters_1vs2 = 0
 for step in range(total_steps):
     control_defenders = single_1vs2_controller_defender(game, value1vs2, grid1vs2)
+    defenders_controls.append(control_defenders.copy())
     # control_defenders = hj_controller_defenders(game, assignments, value1vs1, value2vs1, grid1vs1, grid2vs1)
     control_attackers = hj_contoller_attackers(game, value1vs0, grid1vs0)
+    attackers_controls.append(control_attackers.copy())
     obs, reward, terminated, truncated, info = game.step(np.vstack((control_attackers, control_defenders)))
     
     if terminated or truncated:
@@ -42,3 +44,5 @@ current_status_check(game.attackers_status[-1], step)
 
 #### Animation ####
 animation(game.attackers_traj, game.defenders_traj, game.attackers_status)
+print(f"The controls of defenders are: {defenders_controls} \n")
+print(f"The controls of attackers are: {attackers_controls} \n")
